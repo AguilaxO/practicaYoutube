d9f48d52b93d8ad1e836bcfe10f375b7b60a89a5
aguilax <yo1346>
2023-03-14 23:18:26 -0600

    encabezado

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 
 const hamburguesa = document.querySelector("#encabezado .ListaNavegacion .hamburgesa");
 const lista = document.querySelector('#encabezado .ListaNavegacion ul');
-const encabezado = document.querySelector('#encabezado.container');
+const encabezado = document.querySelector('.encabezado.container');
 
 
 hamburguesa.addEventListener('click', ()=>{
     hamburguesa.classList.toggle('activo');
     lista.classList.toggle('activo');
-})
+});
+
+document.addEventListener('scroll', () => {
+    let posicion = window.scrollY;
+    if (posicion > 250) {
+        encabezado.style.backgroundColor = '#29323c';
+    } else {
+        encabezado.style.backgroundColor = 'transparent';
+    }
+});
